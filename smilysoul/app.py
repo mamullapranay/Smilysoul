7db982548635bbd0612cc93883416aa0657cb557
pranay360 <mamullapranay360@gmail.com>
2024-08-22 19:32:12 +0530

    changes

--- smilysoul/app.py
+++ smilysoul/app.py
@@ -60,9 +60,6 @@ client_secrets_file = {
     }
 }
 
-
-
-
 # Load environment variables and create client secrets file
 client_secrets_file_path = "client_secrets.json"
 
@@ -82,6 +79,7 @@ flowcounsellor = Flow.from_client_secrets_file(
     scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
     redirect_uri="https://smilysoul.onrender.com/authorizecounsellor"
 )
+
 @app.teardown_appcontext
 def teardown_db(exception):
     if hasattr(g, 'mysql_db'):
@@ -141,9 +139,9 @@ def authorize():
         session["mail"] = id_info.get("email")
 
         cur = mysql.connection.cursor()
-        resultvalue = cur.execute("SELECT * FROM USERS WHERE user_id=%s", (session["user"],))
+        resultvalue = cur.execute("SELECT * FROM users WHERE user_id=%s", (session["user"],))
         if resultvalue == 0:
-            cur.execute("INSERT INTO USERS(user_id,email_id,name) VALUES(%s,%s,%s)", (session["user"],session["mail"],session["name"],))
+            cur.execute("INSERT INTO users(user_id,email_id,name) VALUES(%s,%s,%s)", (session["user"],session["mail"],session["name"],))
             mysql.connection.commit()
         cur.close()
 
@@ -166,20 +164,20 @@ def profile():
             cur = mysql.connection.cursor()
 
             if dob != "":
-                cur.execute("UPDATE USERS SET dob=%s WHERE user_id=%s", (dob, session["user"],))
+                cur.execute("UPDATE users SET dob=%s WHERE user_id=%s", (dob, session["user"],))
             else:
-                cur.execute("UPDATE USERS SET dob=NULL WHERE user_id=%s", (session["user"],))
+                cur.execute("UPDATE users SET dob=NULL WHERE user_id=%s", (session["user"],))
 
             if gender:
-                cur.execute("UPDATE USERS SET gender=%s WHERE user_id=%s", (gender, session["user"],))
+                cur.execute("UPDATE users SET gender=%s WHERE user_id=%s", (gender, session["user"],))
             else:
-                cur.execute("UPDATE USERS SET gender=NULL WHERE user_id=%s", (session["user"],))
+                cur.execute("UPDATE users SET gender=NULL WHERE user_id=%s", (session["user"],))
 
             mysql.connection.commit()
             cur.close()
 
         cur = mysql.connection.cursor()
-        resultvalue = cur.execute("SELECT * FROM USERS WHERE user_id=%s", (session["user"],))
+        resultvalue = cur.execute("SELECT * FROM users WHERE user_id=%s", (session["user"],))
         row = cur.fetchone()
         gender = row[4]
         dob = row[3]
@@ -257,14 +255,14 @@ def mysession():
             else:
                 meetlink = "https://meet.google.com/ddf-dmbb-kya"
 
-            cur.execute("INSERT INTO APPOINTMENT(Counsellor_Id, User_ID, Start_Time, Date, meet_link) VALUES(%s, %s, %s, %s, %s)", (counsellor_id, user_id, time, date, meetlink,))
+            cur.execute("INSERT INTO appointment(counsellor_id, user_id, start_time, date, meet_link) VALUES(%s, %s, %s, %s, %s)", (counsellor_id, user_id, time, date, meetlink,))
             cur.execute("UPDATE day_availability SET flag=1 WHERE day_available=%s AND time_slot=%s AND counsellor_id=%s", (day, time, counsellor_id,))
             mysql.connection.commit()
             cur.close()
 
         uid = session["user"]
         cur = mysql.connection.cursor()
-        cur.execute("SELECT * FROM (SELECT * FROM APPOINTMENT NATURAL JOIN counsellor WHERE user_id=%s) AS a", (session["user"],))
+        cur.execute("SELECT * FROM (SELECT * FROM appointment NATURAL JOIN counsellor WHERE user_id=%s) AS a", (session["user"],))
         res = cur.fetchone()
         if res:
             A_date = res[4]
@@ -341,9 +339,9 @@ def authorizecounsellor():
         session["counsellormail"] = id_info.get("email")
 
         cur = mysql.connection.cursor()
-        resultvalue = cur.execute("SELECT * FROM Counsellor WHERE counsellor_id=%s", (session["counsellorid"],))
+        resultvalue = cur.execute("SELECT * FROM counsellor WHERE counsellor_id=%s", (session["counsellorid"],))
         if resultvalue == 0:
-            cur.execute("INSERT INTO Counsellor(counsellor_id, email_id, name, image) VALUES(%s, %s, %s, %s)", (session["counsellorid"], session["counsellormail"], session["counsellorname"], session["counsellorimage"],))
+            cur.execute("INSERT INTO counsellor(counsellor_id, email_id, name, image) VALUES(%s, %s, %s, %s)", (session["counsellorid"], session["counsellormail"], session["counsellorname"], session["counsellorimage"],))
             mysql.connection.commit()
         cur.close()
 
@@ -355,7 +353,7 @@ def counsellor_session():
         return redirect(url_for("home"))
 
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM appointment WHERE Counsellor_id=%s", (session["counsellorid"],))
+    cur.execute("SELECT * FROM appointment WHERE counsellor_id=%s", (session["counsellorid"],))
 
     result = cur.fetchall()
     user = []
@@ -405,7 +403,7 @@ def video():
     if "user" in session:
         uid = session["user"]
         cur = mysql.connection.cursor()
-        cur.execute("SELECT * FROM APPOINTMENT WHERE user_id=%s", (session["user"],))
+        cur.execute("SELECT * FROM appointment WHERE user_id=%s", (session["user"],))
         res = cur.fetchone()
         cur.close()
         roomname = res[0]
@@ -425,7 +423,7 @@ def video():
     elif "counsellorid" in session:
         cid = session["counsellorid"]
         cur = mysql.connection.cursor()
-        cur.execute("SELECT * FROM APPOINTMENT WHERE counsellor_id=%s", (session["counsellorid"],))
+        cur.execute("SELECT * FROM appointment WHERE counsellor_id=%s", (session["counsellorid"],))
         res = cur.fetchone()
         cur.close()
         roomname = res[0]
